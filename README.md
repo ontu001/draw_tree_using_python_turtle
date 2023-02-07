45672bbfa0a4ca26e25f1d4b8f41cabfef6c2c31
ontu001 <mdrohanurrahmanontu@gmail.com>
2023-02-07 10:45:28 -0800

    turtle project added

---
+++ README.md
@@ -0,0 +1,1 @@
+"# draw_tree_using_python_turtle" 
---
+++ main.py
@@ -0,0 +1,28 @@
+import turtle
+
+def draw_tree(branch_length, t):
+    if branch_length > 5:
+        t.forward(branch_length)
+        t.right(20)
+        draw_tree(branch_length-15, t)
+        t.left(40)
+        draw_tree(branch_length-15, t)
+        t.right(20)
+        t.backward(branch_length)
+
+def draw_leaf(t):
+    t.color("green")
+    t.begin_fill()
+    t.circle(10)
+    t.end_fill()
+
+t = turtle.Turtle()
+t.left(90)
+t.up()
+t.backward(100)
+t.down()
+t.color("brown")
+draw_tree(75, t)
+draw_leaf(t)
+
+turtle.exitonclick()
